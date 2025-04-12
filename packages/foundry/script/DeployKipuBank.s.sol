014b3dc0a1103a8a24b3ce68612b43b6aac3de9e
Fernando Shiroma <ryushiroma@gmail.com>
2025-04-12 15:07:32 -0300

    Ajuste em script

---
+++ packages/foundry/script/DeployKipuBank.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../contracts/EdpModulo4.sol";
+
+contract DeployKipuBank is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 initialCap = 100 ether;
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        new KipuBank(initialCap);
+
+        vm.stopBroadcast();
+    }
+}
