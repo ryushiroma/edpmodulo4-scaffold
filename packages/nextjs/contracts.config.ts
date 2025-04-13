437898ab9f32eee97ecaaa46884c07af96935a68
Fernando Shiroma <ryushiroma@gmail.com>
2025-04-12 22:29:36 -0300

    Adiciona config do contrato e atualiza package.json

---
+++ packages/nextjs/contracts.config.ts
@@ -0,0 +1,9 @@
+export const contractConfig = {
+    contracts: {
+      KipuBank: {
+        address: "0x49297d3Cc80b1B265ea026B80dC872d036F7Fff9",
+        abi: require("../../foundry/out/EdpModulo4.sol/KipuBank.json").abi,
+      },
+    },
+  };
+  
